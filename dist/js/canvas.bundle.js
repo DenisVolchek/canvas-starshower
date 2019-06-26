013d710cd51ff145eb1eb07229ac10478587ed43
denisvolchek <volchekdenisminsk@gmail.com>
2019-06-26 18:50:53 +0300

    My commit

--- dist/js/canvas.bundle.js
+++ dist/js/canvas.bundle.js
@@ -103,23 +103,14 @@ var _utils2 = _interopRequireDefault(_utils);
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var canvas = document.querySelector('canvas');
-var c = canvas.getContext('2d');
+var ctx = canvas.getContext('2d');
 
 canvas.width = innerWidth;
 canvas.height = innerHeight;
 
-var mouse = {
-    x: innerWidth / 2,
-    y: innerHeight / 2
-};
-
 var colors = ['#2185C5', '#7ECEFD', '#FFF6E5', '#FF7F66'];
 
 // Event Listeners
-addEventListener('mousemove', function (event) {
-    mouse.x = event.clientX;
-    mouse.y = event.clientY;
-});
 
 addEventListener('resize', function () {
     canvas.width = innerWidth;
@@ -129,44 +120,123 @@ addEventListener('resize', function () {
 });
 
 // Objects
-function Object(x, y, radius, color) {
+function Star(x, y, radius, color) {
     this.x = x;
     this.y = y;
     this.radius = radius;
     this.color = color;
+    this.velocity = {
+        x: 0,
+        y: 3
+    };
+    this.friction = 0.8;
+    this.gravity = 1;
 }
 
-Object.prototype.draw = function () {
-    c.beginPath();
-    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
-    c.fillStyle = this.color;
-    c.fill();
-    c.closePath();
+Star.prototype.draw = function () {
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+    ctx.fillStyle = this.color;
+    ctx.fill();
+    ctx.closePath();
 };
 
-Object.prototype.update = function () {
+Star.prototype.update = function () {
+
+    if (this.y + this.radius + this.velocity.y > canvas.height) {
+        this.velocity.y = -this.velocity.y * this.friction;
+        this.shatter();
+    } else {
+        this.velocity.y += this.gravity;
+    }
+
+    this.x += this.velocity.x;
+    this.y += this.velocity.y;
+
+    this.draw();
+};
+
+Star.prototype.shatter = function () {
+    this.radius -= 3;
+    for (var i = 0; i < 8; i++) {
+        var color = _utils2.default.randomColor(colors);
+        miniStars.push(new MiniStar(this.x, this.y, 2, color));
+    }
+};
+
+function MiniStar(x, y, radius, color) {
+    Star.call(this, x, y, radius, color);
+
+    this.velocity = {
+        x: _utils2.default.randomIntFromRange(-5, 5),
+        y: _utils2.default.randomIntFromRange(-15, 15)
+    };
+    this.friction = 0.8;
+    this.gravity = 0.1;
+    this.ttl = 100;
+    this.opacity = 1;
+}
+
+MiniStar.prototype.draw = function () {
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+    ctx.save();
+    ctx.globalAlpha = this.opacity;
+    ctx.restore();
+    ctx.fillStyle = this.color;
+    ctx.fill();
+    ctx.closePath();
+};
+
+MiniStar.prototype.update = function () {
+
+    if (this.y + this.radius + this.velocity.y > canvas.height) {
+        this.velocity.y = -this.velocity.y * this.friction;
+    } else {
+        this.velocity.y += this.gravity;
+    }
+
+    this.x += this.velocity.x;
+    this.y += this.velocity.y;
+    this.ttl -= 1;
+    this.opacity -= 1 / this.ttl;
+
     this.draw();
 };
 
 // Implementation
-var objects = void 0;
-function init() {
-    objects = [];
+var stars = void 0;
+var miniStars = void 0;
 
-    for (var i = 0; i < 400; i++) {
-        // objects.push()
+function init() {
+    stars = [];
+    miniStars = [];
+
+    for (var i = 0; i < 1; i++) {
+        var star = new Star(canvas.width / 2, canvas.height / 2, 30, 'red');
+
+        stars.push(star);
     }
 }
 
 // Animation Loop
 function animate() {
     requestAnimationFrame(animate);
-    c.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    c.fillText('HTML CANVAS BOILERPLATE', mouse.x, mouse.y);
-    // objects.forEach(object => {
-    //  object.update()
-    // })
+    stars.forEach(function (star, index) {
+        star.update();
+        if (star.radius == 0) {
+            stars.splice(index, 1);
+        }
+    });
+
+    miniStars.forEach(function (miniStar, index) {
+        miniStar.update();
+        if (miniStar.ttl == 0) {
+            miniStars.splice(index, 1);
+        }
+    });
 }
 
 init();
@@ -185,18 +255,18 @@ animate();
 
 
 function randomIntFromRange(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) + min);
+    return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
 function randomColor(colors) {
-  return colors[Math.floor(Math.random() * colors.length)];
+    return colors[Math.floor(Math.random() * colors.length)];
 }
 
 function distance(x1, y1, x2, y2) {
-  var xDist = x2 - x1;
-  var yDist = y2 - y1;
+    var xDist = x2 - x1;
+    var yDist = y2 - y1;
 
-  return Math.sqrt(Math.pow(xDist, 2) + Math.pow(yDist, 2));
+    return Math.sqrt(Math.pow(xDist, 2) + Math.pow(yDist, 2));
 }
 
 module.exports = { randomIntFromRange: randomIntFromRange, randomColor: randomColor, distance: distance };
--- src/js/canvas.js
+++ src/js/canvas.js
@@ -1,23 +1,14 @@
 import utils from './utils'
 
 const canvas = document.querySelector('canvas')
-const c = canvas.getContext('2d')
+const ctx = canvas.getContext('2d')
 
 canvas.width = innerWidth
 canvas.height = innerHeight
 
-const mouse = {
-    x: innerWidth / 2,
-    y: innerHeight / 2
-}
-
 const colors = ['#2185C5', '#7ECEFD', '#FFF6E5', '#FF7F66']
 
 // Event Listeners
-addEventListener('mousemove', event => {
-    mouse.x = event.clientX
-    mouse.y = event.clientY
-})
 
 addEventListener('resize', () => {
     canvas.width = innerWidth
@@ -27,44 +18,123 @@ addEventListener('resize', () => {
 })
 
 // Objects
-function Object(x, y, radius, color) {
+function Star(x, y, radius, color) {
     this.x = x
     this.y = y
     this.radius = radius
     this.color = color
+    this.velocity = {
+        x: 0,
+        y: 3,
+    }
+    this.friction = 0.8
+    this.gravity = 1
 }
 
-Object.prototype.draw = function() {
-    c.beginPath()
-    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
-    c.fillStyle = this.color
-    c.fill()
-    c.closePath()
+Star.prototype.draw = function() {
+    ctx.beginPath()
+    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
+    ctx.fillStyle = this.color
+    ctx.fill()
+    ctx.closePath()
 }
 
-Object.prototype.update = function() {
+Star.prototype.update = function() {
+
+    if(this.y + this.radius + this.velocity.y > canvas.height) {
+        this.velocity.y = -this.velocity.y * this.friction
+        this.shatter()
+    } else {
+        this.velocity.y += this.gravity
+    }
+
+    this.x += this.velocity.x
+    this.y += this.velocity.y
+
+    this.draw()
+}
+
+Star.prototype.shatter = function() {
+    this.radius -= 3
+    for(let i = 0; i < 8; i++) {
+        let color = utils.randomColor(colors)
+        miniStars.push(new MiniStar(this.x, this.y, 2, color))
+    }
+}
+
+function MiniStar(x, y, radius, color) {
+    Star.call(this, x, y, radius, color)
+
+    this.velocity = {
+        x: utils.randomIntFromRange(-5, 5),
+        y: utils.randomIntFromRange(-15, 15),
+    }
+    this.friction = 0.8
+    this.gravity = 0.1
+    this.ttl = 100
+    this.opacity = 1
+}
+
+MiniStar.prototype.draw = function () {
+    ctx.beginPath()
+    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2)
+    ctx.save()
+    ctx.globalAlpha = this.opacity
+    ctx.restore()
+    ctx.fillStyle = this.color
+    ctx.fill()
+    ctx.closePath()
+}
+
+MiniStar.prototype.update = function () {
+
+    if (this.y + this.radius + this.velocity.y > canvas.height) {
+        this.velocity.y = -this.velocity.y * this.friction
+    } else {
+        this.velocity.y += this.gravity
+    }
+
+    this.x += this.velocity.x
+    this.y += this.velocity.y
+    this.ttl -= 1
+    this.opacity -= 1 / this.ttl
+
     this.draw()
 }
 
 // Implementation
-let objects
-function init() {
-    objects = []
+let stars
+let miniStars
 
-    for (let i = 0; i < 400; i++) {
-        // objects.push()
+function init() {
+    stars = []
+    miniStars = []
+
+    for (let i = 0; i < 1; i++) {
+        let star = new Star(canvas.width / 2,canvas.height / 2, 30, 'red' )
+
+        stars.push(star)
     }
 }
 
 // Animation Loop
 function animate() {
     requestAnimationFrame(animate)
-    c.clearRect(0, 0, canvas.width, canvas.height)
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
 
-    c.fillText('HTML CANVAS BOILERPLATE', mouse.x, mouse.y)
-    // objects.forEach(object => {
-    //  object.update()
-    // })
+    stars.forEach((star, index) => {
+        star.update()
+        if(star.radius == 0) {
+            stars.splice(index, 1)
+        }
+    })
+
+    miniStars.forEach((miniStar, index) => {
+        miniStar.update()
+        if (miniStar.ttl == 0) {
+            miniStars.splice(index, 1)
+        }
+    })
 }
 
 init()
